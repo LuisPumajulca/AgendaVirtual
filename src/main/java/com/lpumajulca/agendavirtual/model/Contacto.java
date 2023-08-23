9d5ec761d58f21a863bf26fdb53f797d28e44c2d
LuisPumajulca <pumajulca@hotmail.com>
2023-08-22 21:57:52 -0500

    agregando anotaciones de validaciones de registro

--- src/main/java/com/lpumajulca/agendavirtual/model/Contacto.java
+++ src/main/java/com/lpumajulca/agendavirtual/model/Contacto.java
@@ -5,6 +5,10 @@ import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -18,14 +22,18 @@ public class Contacto {
     @Column(name = "idcontacto")
     private Integer id;
 
+    @NotBlank
     private String nombre;
 
+    @PastOrPresent
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @Column(name = "fechanac")
     private LocalDate fechaNacimiento;
 
+    @Size(max = 15)
     private String celular;
 
+    @Email
     private String email;
 
     @Column(name = "fechareg")
